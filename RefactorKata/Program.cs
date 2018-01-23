eee6c93163fe1b264af17934708275f2c1571fb9
mvdoyle <mvdoyle@uab.edu>
2018-01-23 13:55:22 -0600

    Refactored to best practices and added Product class

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace RefactorKata
 {
@@ -8,34 +9,34 @@ namespace RefactorKata
         static void Main(string[] args)
         {
             //This is intentionally bad : (  Let's Refactor!
-            System.Data.SqlClient.SqlConnection Conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
-
-            System.Data.SqlClient.SqlCommand cmd = Conn.CreateCommand();
-            cmd.CommandText = "select * from Products";
-            /*
-             * cmd.CommandText = "Select * from Invoices";
-             */
-            System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
-            List<Product> products = new List<Product>();
-
-            //TODO: Replace with Dapper
-            while (reader.Read())
+            using (var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
             {
-                var prod = new Product();
-                prod.name = reader["Name"].ToString();
-                products.Add(prod);
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
+
+                var reader = cmd.ExecuteReader();
+                var products = new List<Product>();
+
+                //TODO: Replace with Dapper
+                while (reader.Read())
+                {
+                    var prod = new Product();
+                    prod.Name = reader["Name"].ToString();
+                    products.Add(prod);
+                }
+
+                Console.WriteLine("Products Loaded!");
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);
+                }
             }
-            Conn.Dispose();
-            Console.WriteLine("Products Loaded!");
-            for (int i =0; i< products.Count; i++)
-            {
-                Console.WriteLine(products[i].name);
-            }
+
         }
     }
     public class Product
     {
-        public string name;
-        public string Name { get { return name; } set { name = value; } }
+        public string Name { get; set; }
     }
 }
